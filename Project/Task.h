e11f73fef70027e788d7655757326daa2488b693
Buiypy <cindyyu.0407@gmail.com>
2015-03-22 15:31:00 +0800

    Update Task.h

--- Project/Task.h
+++ Project/Task.h
@@ -4,23 +4,33 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
 
 using namespace std;
 
 class Task
 {
 public:
-	//vector<string> task;
 	Task();
-	Task(string input);
+	Task(string input); // for basic input fomat e.g. add hw1 -from 12:00 -to 13:00 13/3
 	~Task();
-	Task(string task, string input);
+	Task(string task, string input); // for advance format e.g. hw1 13/3 12:00 13:00
 	string ToString();
 	string getTaskname();
 	void UpdateTask(string input);
 	void markAsDone();
 	void markAsUndone();
 	void checkInputValidation();
+	//V0.2
+	string getCertainTaskList(string type); // return certain task group
+	void setPriority(string input);
+	char gerPriority();
+	void changePriority(string request);
+	int charToASCII(char c);
+	char ASCIIToChar(int c);
+	//operator
+	bool operator == (const Task &task2);//const;
+
 	
 private:
 	string taskname;
@@ -31,6 +41,9 @@ private:
 	string scheduled_date;
 	string deadline_date;
 	string status;
+	char priority[1];
+	string task_group;
+	string place;
 };
 
 #endif
