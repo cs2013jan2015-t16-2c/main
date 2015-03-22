bdd9e2b9afa68bcd09c062bf681399e1eea52f65
Zhao Qin <myself_qz@hotmail.com>
2015-03-22 18:34:38 +0800

    change in display date function

--- Project/KeepTrack.cpp
+++ Project/KeepTrack.cpp
@@ -3,15 +3,15 @@
 #include "Task.h"
 #include "storage.h"
 #include "TaskList.h"
+#include <ctime>
 
 int main() {
 	InterfaceOutput::showToUser(InterfaceOutput::displayWelcome());
     cout << "                       Today is ";
     
-    time_t currentTime;
-    time (&currentTime);
-    struct tm * today= localtime(&currentTime);
-    cout << today -> tm_year+1900 << ' ' << m_months[today -> tm_mon + 1] << ' ' << today -> tm_mday << ".\n";
+    time_t t = time(0);
+    struct tm * now = localtime(&t);
+    cout << now -> tm_year + 1900 << '-' << now -> tm_mon + 1 << '-' << now -> tm_mday << endl;
 	
     cout << "================================================================================\n";
     
