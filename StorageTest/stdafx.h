e52c62e1957f28815f1827628091b633aba3440d
Cai Yu <caizhelun000@hotmail.com>
2015-04-13 21:52:53 +0800

    unit testing

---
+++ StorageTest/stdafx.h
@@ -0,0 +1,22 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+// Headers for CppUnitTest
+#include "CppUnitTest.h"
+
+// TODO: reference additional headers your program requires here
+#include "..\main\Project\DisplayedTaskList.h"
+#include "..\main\Project\InterfaceInput.h"
+#include "..\main\Project\InterfaceOutput.h"
+#include "..\main\Project\Storage.h"
+#include "..\main\Project\Task.h"
+#include "..\main\Project\TaskList.h"
+#include "..\main\Project\Help.h"
+#include "..\main\Project\MagicString.h"
+#include "..\main\Project\DisplayedTaskList.h"
